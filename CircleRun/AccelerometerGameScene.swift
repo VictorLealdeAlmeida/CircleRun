cba4ee3e592c97377cb810cf7d1048af59fed82a
VictorLealdeAlmeida <vlpaa@cin.ufpe.br>
2017-01-11 15:21:33 -0300

    Mundanca para o giroscopio

--- CircleRun/AccelerometerGameScene.swift
+++ CircleRun/AccelerometerGameScene.swift
@@ -20,18 +20,22 @@ extension GameScene{
                 let currentX = self.player.position.x
 
                 if (data!.acceleration.x < 0.0) {
-                    if (currentX + CGFloat((data?.acceleration.x)! * 100)) > -300{
+                    if (currentX + CGFloat((data?.acceleration.x)! * 100)) > self.limitLeft{
                         self.destX = currentX + CGFloat((data?.acceleration.x)! * 100)
+                    }else{
+                        self.destX = self.limitLeft - 1
                     }
                 }
                     
                 else if data!.acceleration.x > 0 {
-                    if (currentX + CGFloat((data?.acceleration.x)! * 100)) < 300{
+                    if (currentX + CGFloat((data?.acceleration.x)! * 100)) < self.limitRight{
                         self.destX = currentX + CGFloat((data?.acceleration.x)! * 100)
+                    }else{
+                        self.destX = self.limitRight - 1
                     }
                 }
                 
-                  //  print(self.destX)
+                   // print(self.destX)
                 
             })
             
--- CircleRun/GameScene.swift
+++ CircleRun/GameScene.swift
@@ -24,29 +24,71 @@ struct PhysicsCategory {
 class GameScene: SKScene, SKPhysicsContactDelegate{
     
     var player = SKSpriteNode(imageNamed: "circle")
-    var motionManager = CMMotionManager()
+    //var motionManager = CMMotionManager()
     var destX:CGFloat  = 0.0
+    
+    var limitRight : CGFloat = 0.0
+    var limitLeft : CGFloat = 0.0
+    
+    var motionManager : CMMotionManager = {
+        let motion = CMMotionManager()
+        motion.accelerometerUpdateInterval = 0.01
+        
+        return motion
+        
+    }()
+    
+   
     
     override func didMove(to view: SKView) {
         //Gravidade do jogo, no caso 0
         physicsWorld.gravity = CGVector(dx: 0, dy: 0)
         physicsWorld.contactDelegate = self
         
-        createPlayer()
-        motionPlayer()
+        limitRight = 0.8*(size.width/2)
+        limitLeft = -0.8*(size.width/2)
+        print(limitRight)
         
+        createPlayer()
+      //  motionPlayer()
+        
+        self.motionManager.startGyroUpdates(to: OperationQueue.current!) { (gyroData: CMGyroData?, NSError) in
+            self.moveRacket()
+            if(NSError != nil) {
+                print("\(NSError)")
+            }
+            
+        }
         
     }
     
     
     override func update(_ currentTime: TimeInterval) {
-        if destX < 300 && destX > -300{
-            let action = SKAction.moveTo(x: destX, duration: 0.1)
-            self.player.run(action)
-        }
+      //  if destX < limitLeft && destX > limitRight{
+       //     let action = SKAction.moveTo(x: destX, duration: 0.1)
+        //    self.player.run(action)
+        //}
         
-            //print("----")
     }
     
+    func moveRacket() {
+        
+        let yForce = self.motionManager.gyroData!.rotationRate.y
+        print(yForce)
+        let dxVelocity = self.player.physicsBody?.velocity.dx
+        self.player.physicsBody?.velocity.dx = dxVelocity! + 32*CGFloat(yForce)
+        
+        //"lock" the racket on the game area
+        
+        if (player.position.x) > self.frame.maxX - ((player.size.width)){
+            player.position.x = self.frame.maxX - ((player.size.width))
+        }
+        if (player.position.x) < self.frame.minX + ((player.size.width)){
+            player.position.x = self.frame.minX + ((player.size.width))
+        }
+        
+        
+    }
+
     
 }
